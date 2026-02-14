3a3a1bc55cc966fbb400736684f49e57ccb5c255
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-02-14 15:48:54 +0400

    added css color theme for the future

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,25 @@ module.exports = {
   content: [
     './src/**/*.{html,js,ts,jsx,tsx}',
   ],
-  // theme: {
-  //   extend: {
-  //     typography: {
-  //       DEFAULT: {
-  //         css: {
-  //           p: {
-  //             margin: '0.25rem 0', // my-1
-  //           },
-  //           li: {
-  //             margin: '0.25rem 0', // my-1
-  //           },
-  //           h1: { margin: '1rem 0 0.5rem 0' },
-  //           h2: { margin: '0.75rem 0 0.5rem 0' },
-  //           h3: { margin: '0.5rem 0 0.25rem 0' },
-  //         }
-  //       }
-  //     }
-  //   }
-  // },
+  theme: {
+    extend: {
+      // typography: {
+      //   DEFAULT: {
+      //     css: {
+      //       p: {
+      //         margin: '0.25rem 0', // my-1
+      //       },
+      //       li: {
+      //         margin: '0.25rem 0', // my-1
+      //       },
+      //       h1: { margin: '1rem 0 0.5rem 0' },
+      //       h2: { margin: '0.75rem 0 0.5rem 0' },
+      //       h3: { margin: '0.5rem 0 0.25rem 0' },
+      //     }
+      //   }
+      // }
+    },
+  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
